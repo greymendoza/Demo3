b2c41982f27a0064b8dffcaa8e6550e42b9a891c
greys <greys@192.168.1.236>
2023-01-12 20:05:16 -0500

    Method correction

--- src/demo3package/DemoClass.java
+++ src/demo3package/DemoClass.java
@@ -11,5 +11,7 @@ public class DemoClass {
 		
 		System.out.println(a + " + " + b + " = " + (a + b));
 	}
-	public static void add (int a, int b, int c)
+	public static void add (int a, int b, int c) {
+		
+	}
 }
