b8b44c9caefddc56a22fe14678f18dfb3dd0ce7f
greys <greys@192.168.1.236>
2023-01-12 19:34:34 -0500

    2nd Commit

--- src/demo3package/DemoClass.java
+++ src/demo3package/DemoClass.java
@@ -1,7 +1,14 @@
 package demo3package;
 
 public class DemoClass {
-public static void main(String[] args) {
-	System.out.println("Practice");
-}
+	public static void main(String[] args) {
+		System.out.println("Practice ");
+
+	}
+	public static void add (int a, int b) {
+		a = 1;
+		b = 1;
+		
+		System.out.println(a + " + " + b + " = " + (a + b));
+	}
 }
